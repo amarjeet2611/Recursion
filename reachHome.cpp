5605e4e4e3ec75be931b9849cd0ff440a127408a
amarjeet2611 <123499426+amarjeet2611@users.noreply.github.com>
2023-07-27 03:28:55 +0530

    first commit

---
+++ reachHome.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void reachHome(int src,int dest){
+    cout<<"source: "<<src<<" destination: "<<dest<<endl;
+//base case
+if(src==dest){
+    cout<<"Reached"<<endl;
+    return ;
+}
+src++;
+reachHome(src,dest);
+}
+
+int main(){
+int dest=10;
+int src=1;
+reachHome(src,dest);
+return 0;
+}
